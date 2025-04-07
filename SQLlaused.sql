e85ae645b0c8597a7cbe227edc0ce25b5708736f
DoM1Nity <153494095+DoM1Nity@users.noreply.github.com>
2025-04-07 11:04:11 +0300

    tabeli loomine SQL laused

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,1 +1,25 @@
+--kommentaar 
+--SQL SERVER Managment Stuudio 
+--Conect TO:
+--Server Name: (localdb)\MSSQLLocalDB
+-- Authentification 2 tüüpi:
+--1. Windows Auth - Localdb admini õigused
+--2. SQL Server Authentification - kontrollida varem tehtud kasutajad
 
+CREATE DATABASE DanikLOGITvp23;
+USE DanikLOGITvp23;
+
+--tabeli loomine
+--identity(1,1) - ise täidab tabeli 1,2,3,...
+CREATE TABLE inimene(
+inimineID int Primary Key identity (1,1),
+nimi varchar(50) unique,
+synniaeg date,
+telefon char(12),
+pikkus decimal(5,2),
+opilaskodu bit
+);
+SELECT * FROM inimene;
+
+--tabeli kustutamine
+DROP table inimene;
